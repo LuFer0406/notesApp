e0287a02c2e99fb7385b5856471a8627b1fd5569
LuisaFernanda <luisamoralesocp@gmail.com>
2023-02-28 16:43:10 -0500

    notesApp with .env

--- src/database.js
+++ src/database.js
@@ -2,11 +2,9 @@ import mongoose from "mongoose";
 
 mongoose.set('strictQuery', false);
 
-const uri = "mongodb+srv://root:1234@cluster0.iifcrfm.mongodb.net/db_notesapp";
-
 export const connectDb = async() => {
     try {
-        const db = await mongoose.connect(uri);
+        const db = await mongoose.connect(process.env.MONGO_URI);
         console.log(`La base de datos ${db.connection.name} ha sido conectada correctamente.`)
     } catch (error) {
         console.log(`Ha sucedido un error al conectar a la base de datos ${error.message}`)
--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
+import * as dotenv from 'dotenv'
 import express from "express";
 import { connectDb } from "./database.js";
 import { socket } from "./socket/noteSocket.js";
 const app = express();
 
+dotenv.config();
+
 connectDb();
 
-app.set("Port", 4000);
+app.set("Port", process.env.PORT);
 
 const server = app.listen(app.get("Port"), () => {
   console.log("El servidor está escuchando por el puerto", app.get("Port"));
